a593071298c4f5ce71eed4d14966b3758c6fdef0
salcrom <seya.28@gmail.com>
2024-09-09 12:10:26 +0200

    READme update

--- 01-fundamentos/assets/js/bases/alerts.js
+++
@@ -1,9 +0,0 @@
-// alert("Hola Mundo");
-
-// let nombre = prompt("¿Cuál es tu nombre?");
-// console.log("****" + nombre + "****");
-
-// const seleccion = confirm("¿Estás seguro de borrar esto?");
-// console.log(seleccion);
-
-console.log(global); // global es como 'window' en el navegador pero en la terminal
